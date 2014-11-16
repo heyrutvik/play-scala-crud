785254be1b56e535e50efa01c78e8e8faa404f4a
heyrutvik <heyrutvik@gmail.com>
2014-11-16 14:03:01 +0530

    routes defined.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,8 +5,9 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
+    def index = Action {
+        // redirect to list of tasks
+        Redirect(routes.Task.tasks)
+    }
 
 }
---
+++ app/controllers/Task.scala
@@ -0,0 +1,20 @@
+package controllers
+
+import  play.api._
+import  play.api.mvc._
+
+object Task extends Controller {
+
+    def tasks = TODO
+
+    def create = TODO
+
+    def read(id: Long) = TODO
+
+    def edit(id: Long) = TODO
+
+    def update(id: Long) = TODO
+
+    def delete(id: Long) = TODO
+
+}
